78f8825e0b9378b49e7746e8f7c89cd0e0db8ef7
Remulo Costa <remulo.costa@gmail.com>
2020-05-26 22:23:05 -0300

    modified date appointment test

--- src/modules/appointments/services/CreateAppointmenteService.spec.ts
+++ src/modules/appointments/services/CreateAppointmenteService.spec.ts
@@ -25,7 +25,7 @@ describe('CreateAppointment', () => {
       fakeAppointmentRepository,
     );
 
-    const appointmentDate = new Date(2020, 5, 26, 23);
+    const appointmentDate = new Date(2020, 5, 26, 22);
 
     await CreateAppointment.execute({
       date: appointmentDate,
